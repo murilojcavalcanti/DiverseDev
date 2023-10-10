aac6476a0ea51aa0dd0d4c235b77eac6c1c87f1b
murilojcavalcanti <josemurilo7@gmail.com>
2023-10-09 22:12:56 -0300

    classe exercicios criada

--- prova/Program.cs
+++ prova/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using prova.questoes;
+exercicios exercicios = new exercicios();
+int opcao=0;
+do
+{
+    try
+    {
+        ExibirMenu();
+        Console.Write("Digite a opção escolhida: ");
+        bool isnumeric = int.TryParse(Console.ReadLine(), out opcao);
+        if (!isnumeric)
+        {
+            Console.WriteLine("Entrada de dado invalida! Digite um numero!");
+        }
+        else
+        {
+            menu(opcao);
+        }
+        Console.Clear();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Mensagem de erro: {ex.Message}");
+    }
+} while (opcao != 0);
+
+
+
+
+void ExibirMenu()
+{
+    Console.WriteLine("===== Menu de Exercicios =====");
+    Console.WriteLine("\nDigite 1 para o Exercicio 1: ");
+    Console.WriteLine("Digite 2 para o Exercicio 2: ");
+    Console.WriteLine("Digite 3 para o Exercicio 3: ");
+    Console.WriteLine("Digite 4 para o Exercicio 4: ");
+    Console.WriteLine("Digite 0 para sair: ");
+}
+
+void menu(int opcao)
+{
+
+    switch (opcao)
+    {
+        case 1:
+            exercicios.exercicio01();
+            Console.WriteLine("Aperte uma tecla para voltar ao menu");
+            Console.ReadKey();
+            break;
+        case 2:
+            exercicios.exercicio02();
+            Console.WriteLine("Aperte uma tecla para voltar ao menu");
+            Console.ReadKey();
+            break;
+        case 3:
+            exercicios.exercicio03();
+            Console.WriteLine("Aperte uma tecla para voltar ao menu");
+            Console.ReadKey();
+            break;
+        case 4:
+            exercicios.exercicio04();
+            Console.WriteLine("Aperte uma tecla para voltar ao menu");
+            Console.ReadKey();
+            break;
+
+        default:
+            Console.WriteLine("Digite uma opção valida");
+            break;
+    }
+}
---
+++ prova/questoes/exercicios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova.questoes
+{
+    public class exercicios
+    {
+        internal void exercicio01()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void exercicio02()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void exercicio03()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void exercicio04()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
